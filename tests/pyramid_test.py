889a3cc670fab7e50d94b655d62f05aa767ab677
Marcelo Moraes <marcelo.moraes@geru.com.br>
2018-09-15 17:41:38 -0300

    Using fixture conf_request

--- tests/pyramid_test.py
+++ tests/pyramid_test.py
@@ -2,11 +2,7 @@ import pytest
 
 from fbroadrunner.errors import FbRoadRunnerFieldError
 from fbroadrunner.pyramid import fb_messenger, fb_publication
-
-
-class Request(object):
-    def __init__(self, post):
-        self.POST = post
+from .fixtures import conf_request
 
 
 @fb_messenger(app_id=12345678, link='https://fbrunner.com', redirect_uri='https://fbrunner.com')
@@ -39,66 +35,66 @@ def example_fb_publication_without_parameters(request, **kwargs):
     return kwargs['fb_url']
 
 
-def test_fb_publication_basic(mocker):
-    request = Request(post={})
+def test_fb_publication_basic(mocker, conf_request):
+    request = conf_request(post={})
     fb_pub = example_fb_publication(request=request)
     assert fb_pub == 'https://www.facebook.com/dialog/feed?app_id=12345678&link=https%3A%2F%2Ffbrunner.com&redirect_uri=https%3A%2F%2Ffbrunner.com'
 
 
-def test_fb_publication_with_one_parameter_with_post(mocker):
-    request = Request(post={"fb_redirect_uri": "http://road.com"})
+def test_fb_publication_with_one_parameter_with_post(mocker, conf_request):
+    request = conf_request(post={"fb_redirect_uri": "http://road.com"})
     fb_pub = example_fb_publication(request=request)
     assert fb_pub == 'https://www.facebook.com/dialog/feed?app_id=12345678&link=https%3A%2F%2Ffbrunner.com&redirect_uri=https%3A%2F%2Ffbrunner.com'
 
 
-def test_fb_publication_raises(mocker):
-    request = Request(post={"fb_link": "http://road.com", "fb_redirect_uri": "http://road.com"})
+def test_fb_publication_raises(mocker, conf_request):
+    request = conf_request(post={"fb_link": "http://road.com", "fb_redirect_uri": "http://road.com"})
     with pytest.raises(FbRoadRunnerFieldError):
         example_fb_publication_without_app_id(request=request)
 
 
-def test_fb_publication_with_env(monkeypatch):
+def test_fb_publication_with_env(monkeypatch, conf_request):
     monkeypatch.setenv('FB_APP_ID', 123456789)
-    request = Request(post={})
+    request = conf_request(post={})
     fb_pub = example_fb_publication_without_app_id(request=request)
     assert fb_pub == 'https://www.facebook.com/dialog/feed?app_id=123456789&link=https%3A%2F%2Ffbrunner.com&redirect_uri=https%3A%2F%2Ffbrunner.com'
 
 
-def test_example_fb_publication_with_all_parameters_with_post():
-    request = Request(
+def test_example_fb_publication_with_all_parameters_with_post(conf_request):
+    request = conf_request(
         post={"fb_redirect_uri": "http://road.com", "display": "popup&amp;caption=An%20example%20caption",
               "to": "1234", "from": "7575", "source": "12345", "link": "https://roadrunner.com"})
     fb_pub = example_fb_publication_without_parameters(request=request)
     assert fb_pub == 'https://www.facebook.com/dialog/feed?display=popup%26amp%3Bcaption%3DAn%2520example%2520caption&to=1234&from=7575&source=12345&link=https%3A%2F%2Froadrunner.com&app_id=12345678'
 
 
-def test_fb_messenger_basic(mocker):
-    request = Request(post={})
+def test_fb_messenger_basic(conf_request):
+    request = conf_request(post={})
     fb_message = example_fb_messenger(request=request)
     assert fb_message == 'http://www.facebook.com/dialog/send?app_id=12345678&redirect_uri=https%3A%2F%2Ffbrunner.com&link=https%3A%2F%2Ffbrunner.com'
 
 
-def test_fb_messenger_with_one_parameter_with_post(mocker):
-    request = Request(post={"fb_redirect_uri": "http://road.com"})
+def test_fb_messenger_with_one_parameter_with_post(conf_request):
+    request = conf_request(post={"fb_redirect_uri": "http://road.com"})
     fb_message = example_fb_messenger(request=request)
     assert fb_message == 'http://www.facebook.com/dialog/send?app_id=12345678&redirect_uri=https%3A%2F%2Ffbrunner.com&link=https%3A%2F%2Ffbrunner.com'
 
 
-def test_fb_messenger_raises(mocker):
-    request = Request(post={"fb_link": "http://road.com", "fb_redirect_uri": "http://road.com"})
+def test_fb_messenger_raises(conf_request):
+    request = conf_request(post={"fb_link": "http://road.com", "fb_redirect_uri": "http://road.com"})
     with pytest.raises(FbRoadRunnerFieldError):
         example_fb_messenger_without_app_id(request=request)
 
 
-def test_fb_messenger_with_env(monkeypatch):
+def test_fb_messenger_with_env(monkeypatch, conf_request):
     monkeypatch.setenv('FB_APP_ID', 123456789)
-    request = Request(post={})
+    request = conf_request(post={})
     fb_message = example_fb_messenger_without_app_id(request=request)
     assert fb_message == 'http://www.facebook.com/dialog/send?app_id=123456789&redirect_uri=https%3A%2F%2Ffbrunner.com&link=https%3A%2F%2Ffbrunner.com'
 
 
-def test_example_fb_messenger_with_all_parameters_with_post():
-    request = Request(
+def test_example_fb_messenger_with_all_parameters_with_post(conf_request):
+    request = conf_request(
         post={"fb_redirect_uri": "http://road.com", "display": "popup&amp;caption=An%20example%20caption",
               "to": "1234", "link": "https://roadrunner.com"})
     fb_message = example_fb_messenger_without_parameters(request=request)
