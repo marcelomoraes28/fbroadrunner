8e3de61588db57f0bf8083e6a165ea5c3924de60
Marcelo Moraes <marcelo.moraes@geru.com.br>
2018-09-15 21:31:24 -0300

    Update release version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ extras_require = {
 
 setup(
     name='fbroadrunner',
-    version='0.0.1-alpha',
+    version='0.0.2-alpha',
     description='Facebook Road Runner',
     classifiers=[
         'Development Status :: 3 - Alpha',
